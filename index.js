f8118afac932ec7ce7e5302ad0036e28a22f3d25
Nathan Orick <cnathanorick@gmail.com>
2018-06-04 11:11:03 -0400

    Update sdk and remove inlined code

--- index.js
+++ index.js
@@ -254,17 +254,6 @@ const handlers = {
             console.info("Ending getAddressHandler()");
         });
     },
-    // SDK is broken. This is here until it is fixed.
-    ':tellWithPermissionCard': function (speechOutput, permissions) {
-        this.handler.response = buildSpeechletResponse({
-            sessionAttributes: this.attributes,
-            output: getSSMLResponse(speechOutput),
-            permissions: permissions,
-            type: 'AskForPermissionsConsent',
-            shouldEndSession: true
-        });
-        this.emit(':responseReady');
-    }
 };
 
 exports.handler = function (event, context) {
@@ -273,95 +262,6 @@ exports.handler = function (event, context) {
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
-
-// Also here to support the fix for the broken sdk.
-function getSSMLResponse(message) {
-    if (message == null) {
-        return null;
-    } else {
-        return {
-            type: 'SSML',
-            speech: `<speak> ${message} </speak>`
-        };
-    }
-}
-function createSpeechObject(optionsParam) {
-    if (optionsParam && optionsParam.type === 'SSML') {
-        return {
-            type: optionsParam.type,
-            ssml: optionsParam['speech']
-        };
-    } else {
-        return {
-            type: optionsParam.type || 'PlainText',
-            text: optionsParam['speech'] || optionsParam
-        };
-    }
-}
-function buildSpeechletResponse(options) {
-    var alexaResponse = {
-        shouldEndSession: options.shouldEndSession
-    };
-
-    if (options.output) {
-        alexaResponse.outputSpeech = createSpeechObject(options.output);
-    }
-
-    if (options.reprompt) {
-        alexaResponse.reprompt = {
-            outputSpeech: createSpeechObject(options.reprompt)
-        };
-    }
-
-    if (options.directives) {
-        alexaResponse.directives = options.directives;
-    }
-
-    if (options.cardTitle && options.cardContent) {
-        alexaResponse.card = {
-            type: 'Simple',
-            title: options.cardTitle,
-            content: options.cardContent
-        };
-
-        if (options.cardImage && (options.cardImage.smallImageUrl || options.cardImage.largeImageUrl)) {
-            alexaResponse.card.type = 'Standard';
-            alexaResponse.card['image'] = {};
-
-            delete alexaResponse.card.content;
-            alexaResponse.card.text = options.cardContent;
-
-            if (options.cardImage.smallImageUrl) {
-                alexaResponse.card.image['smallImageUrl'] = options.cardImage.smallImageUrl;
-            }
-
-            if (options.cardImage.largeImageUrl) {
-                alexaResponse.card.image['largeImageUrl'] = options.cardImage.largeImageUrl;
-            }
-        }
-    } else if (options.cardType === 'LinkAccount') {
-        alexaResponse.card = {
-            type: 'LinkAccount'
-        };
-    } else if (options.cardType === 'AskForPermissionsConsent') {
-        alexaResponse.card = {
-            type: 'AskForPermissionsConsent',
-            permissions: options.permissions
-        };
-    }
-
-    var returnResult = {
-        version: '1.0',
-        response: alexaResponse
-    };
-
-    if (options.sessionAttributes) {
-        returnResult.sessionAttributes = options.sessionAttributes;
-    }
-    return returnResult;
-}
-// End SDK fixes
-
 
 
 /**
